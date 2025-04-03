5f4a629e3e76b3cd55104ed5f09ff6ad86462d7b
creallfluharty <creallfluharty@gmail.com>
2025-04-02 23:35:25 -0500

    let there be music

--- src/edu/missouri/groupn/mlm/Main.java
+++ src/edu/missouri/groupn/mlm/Main.java
@@ -1,7 +1,66 @@
 package edu.missouri.groupn.mlm;
 
+import java.util.HashMap;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.ShortMessage;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("do you hear the music?");
+		try {
+			System.out.println("do you hear the music?");
+			var events = MidiCsvParser.parseCsv("media/mystery_song.csv");
+			
+			var channelMap = new HashMap<Integer, Integer>();
+			for (var event : events) {
+				channelMap.put(event.getChannel(), event.getInstrument());
+			}
+			
+			var sequence = new Sequence(Sequence.PPQ, 384);
+
+			var track = sequence.createTrack();
+			for (var channel : channelMap.keySet()) {
+				track.add(
+					new MidiEvent(
+						new ShortMessage(
+							ShortMessage.PROGRAM_CHANGE,
+							0,
+							channelMap.get(channel)
+						),
+						0
+					)
+				);
+			}
+
+			for (var event : events) {
+				track.add(
+					new MidiEvent(
+						new ShortMessage(
+							event.getNoteOnOff(),
+							event.getNote(),
+							event.getVelocity()
+						),
+						event.getStartEndTick()
+					)
+				);
+			}
+			
+			var sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			sequencer.setSequence(sequence);
+			sequencer.start();
+			
+			while (sequencer.isRunning()) {
+				Thread.sleep(100);
+			}
+			
+			sequencer.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/edu/missouri/groupn/mlm/MidiEventData.java
+++ src/edu/missouri/groupn/mlm/MidiEventData.java
@@ -29,100 +29,51 @@ public class MidiEventData {
 		this.instrument = instrument;
 	}
 
-	/**
-	 * This getter method returns an integer with the startEndTick value of the MIDI event it's called upon.
-	 * @return MIDI event's startEndTick value
-	 */
 	public int getStartEndTick() {
 		return startEndTick;
 	}
 
-	/**
-	 * This setter method updates the startEndTick value of the MIDI event it's called upon with the passed argument's value.
-	 * @param startEndTick The startEndTick value to be set for a MIDI event
-	 */
 	public void setStartEndTick(int startEndTick) {
 		this.startEndTick = startEndTick;
 	}
 
-	/**
-	 * This getter method returns an integer with the velocity value of the MIDI event it's called upon.
-	 * @return MIDI event's velocity value
-	 */
 	public int getVelocity() {
 		return velocity;
 	}
 
-	/**
-	 * This setter method updates the velocity value of the MIDI event it's called upon with the passed argument's value.
-	 * @param velocity The velocity value to be set for a MIDI event
-	 */
 	public void setVelocity(int velocity) {
 		this.velocity = velocity;
 	}
 
-	/**
-	 * This getter method returns an integer with the note value of the MIDI event it's called upon.
-	 * @return MIDI event's note value
-	 */
 	public int getNote() {
 		return note;
 	}
 
-	/**
-	 * This setter method updates the note value of the MIDI event it's called upon with the passed argument's value.
-	 * @param note The note value to be set for a MIDI event
-	 */
 	public void setNote(int note) {
 		this.note = note;
 	}
 
-	/**
-	 * This getter method returns an integer with the channel value of the MIDI event it's called upon.
-	 * @return MIDI event's channel value
-	 */
 	public int getChannel() {
 		return channel;
 	}
 
-	/**
-	 * This setter method updates the channel value of the MIDI event it's called upon with the passed argument's value.
-	 * @param channel The channel value to be set for a MIDI event
-	 */
 	public void setChannel(int channel) {
 		this.channel = channel;
 	}
 
-	/**
-	 * This getter method returns an integer with the noteOnOff value of the MIDI event it's called upon.
-	 * @return MIDI event's noteOnOff value
-	 */
 	public int getNoteOnOff() {
 		return noteOnOff;
 	}
 
-	/**
-	 * This setter method updates the noteOnOff value of the MIDI event it's called upon with the passed argument's value.
-	 * @param noteOnOff The noteOnOff value to be set for a MIDI event
-	 */
 	public void setNoteOnOff(int noteOnOff) {
 		this.noteOnOff = noteOnOff;
 	}
 
-	/**
-	 * This getter method returns an integer with the instrument value of the MIDI event it's called upon.
-	 * @return MIDI event's instrument value
-	 */
 	public int getInstrument() {
 		return instrument;
 	}
 
-	/**
-	 * This setter method updates the instrument value of the MIDI event it's called upon with the passed argument's value.
-	 * @param instrument The instrument value to be set for a MIDI event
-	 */
 	public void setInstrument(int instrument) {
 		this.instrument = instrument;
-	}
-	
+	}	
 }
